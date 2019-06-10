add1ac408868d6a19c6048c846174cf73745a0a8
Yolotrs <brunogiflype.sb@gmail.com>
2019-06-10 09:46:52 -0300

    TELA DE BUSCA

---
+++ Helpets/app/src/main/java/pad/time3/helpets/info.kt
@@ -0,0 +1,12 @@
+package pad.time3.helpets
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class info : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info2)
+    }
+}
